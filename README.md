68e0a740d8a5402fea15d84f5003d38db4a9a772
Jay Stewart <jay@jaystewart.co.uk>
2023-05-10 15:47:57 -0600

    New: parameters, ideas, exercises, progress bars and refactoring

--- README.md
+++ README.md
@@ -6,23 +6,36 @@ A simple screen break program. "Step away" typically means to physically remove 
 - This is hardcoded to 20 second breaks every 10 minutes and 5 minute breaks every 30 minutes.
 
 ## Requirements
-Pygame is used for the break screen
+pygame is used for the break screen
 
 `pip install pygame`
 
-Playerctl is used to pause media
+playerctl is used to pause media
 
 `sudo apt install playerctl`
+
+tqdm is used to quickly see how (long) until next break
+
+`pip install tqdm`
 
 ## Usage
 `python main.py`
 
 ```
-usage: main.py [-h] [--take_break TAKE_BREAK]
+usage: main.py [-h] [--short_break_freq_min SHORT_BREAK_FREQ_MIN] [--short_break_length_sec SHORT_BREAK_LENGTH_SEC] [--long_break_freq_min LONG_BREAK_FREQ_MIN]
+               [--long_break_length_min LONG_BREAK_LENGTH_MIN] [--break_now BREAK_NOW]
 
 options:
   -h, --help            show this help message and exit
-  --take_break TAKE_BREAK
+  --short_break_freq_min SHORT_BREAK_FREQ_MIN
+                        Short break frequency in minutes
+  --short_break_length_sec SHORT_BREAK_LENGTH_SEC
+                        Short break length in minutes
+  --long_break_freq_min LONG_BREAK_FREQ_MIN
+                        Long break frequency in minutes
+  --long_break_length_min LONG_BREAK_LENGTH_MIN
+                        Long break length in minutes
+  --break_now BREAK_NOW
                         Start with a break in minutes
 ```
 
@@ -30,9 +43,10 @@ options:
 Only tested on Ubuntu 22.04.2 LTS.
 
 ## :exploding_head: Contribution Ideas :exploding_head:
-1. As a screen addict, I want configure my break times so that I don't go square eyed.
+1. Create a configuration class to store and manage the configuration settings
+2. Create a testing plan
+3. Create a system tray icon
 
 ## Credits
-- "gong.wav" by InspectorJ ([www.jshaw.co.uk](https://www.jshaw.co.uk)) of [Freesound.org](http://freesound.org/)
 - Exercises by University Health Service of [University of Michigan](https://uhs.umich.edu/computerergonomics)
 - Inspired by [Safe Eyes](https://github.com/slgobinath/SafeEyes)
--- main.py
+++ main.py
@@ -6,23 +6,28 @@ import time
 import subprocess
 import json
 import random
+from tqdm import tqdm
 
 
 class StepAway:
-    short_freq_min = None
-    short_length_s = None
-    long_freq_min = None
-    long_length_min = None
+    short_break_freq_min = None
+    short_break_length_sec = None
+    long_break_freq_min = None
+    long_break_length_min = None
     args = []
     current_break_type = None
 
     def __init__(
-        self, short_freq_min=10, short_length_s=20, long_freq_min=30, long_length_min=5
+        self,
+        short_break_freq_min=10,
+        short_break_length_sec=20,
+        long_break_freq_min=30,
+        long_break_length_min=5,
     ) -> None:
-        self.short_freq_min = short_freq_min
-        self.short_length_s = short_length_s
-        self.long_freq_min = long_freq_min
-        self.long_length_min = long_length_min
+        self.short_break_freq_min = short_break_freq_min
+        self.short_break_length_sec = short_break_length_sec
+        self.long_break_freq_min = long_break_freq_min
+        self.long_break_length_min = long_break_length_min
 
         logging.basicConfig(
             level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -30,79 +35,100 @@ class StepAway:
         logging.info("Welcome to Step Away!")
 
         parser = argparse.ArgumentParser()
-        parser.add_argument("--take_break", help="Start with a break in minutes")
+        parser.add_argument(
+            "--short_break_freq_min", help="Short break frequency in minutes"
+        )
+        parser.add_argument(
+            "--short_break_length_sec", help="Short break length in minutes"
+        )
+        parser.add_argument(
+            "--long_break_freq_min", help="Long break frequency in minutes"
+        )
+        parser.add_argument(
+            "--long_break_length_min", help="Long break length in minutes"
+        )
+        parser.add_argument("--break_now", help="Start with a break in minutes")
         self.args = parser.parse_args()
 
-    def take_break(self, s):
-        self.pause_media_player()
-        pygame.init()
-        screen_size = pygame.display.Info().current_w, pygame.display.Info().current_h
-        screen = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
-        screen.fill((0, 0, 0))
-        self.show_message(screen)
-        pygame.display.update()
-        logging.info(
-            f"Taking a {str(s if s < 60 else int(s/60))} {'second' if s < 60 else 'minute'} break"
-        )
-        time.sleep(s)
-        pygame.quit()
-        os.system("aplay gong.wav")
+        if self.args.short_break_freq_min:
+            self.short_break_freq_min = int(self.args.short_break_freq_min)
+        if self.args.short_break_length_sec:
+            self.short_break_length_sec = int(self.args.short_break_length_sec)
+        if self.args.long_break_freq_min:
+            self.long_break_freq_min = int(self.args.long_break_freq_min)
+        if self.args.long_break_length_min:
+            self.long_break_length_min = int(self.args.long_break_length_min)
 
     def get_random_exercise(self):
         with open("exercises.json", "r") as f:
             exercises = json.load(f)
+        random.seed(time.time())
         return random.choice(exercises)
 
-    def show_message(self, screen):
-        font_size = 32
-        font = pygame.font.Font(None, font_size)
-        line1 = font.render(self.current_break_type, True, (255, 255, 255))
-        line2 = font.render(self.get_random_exercise(), True, (255, 255, 255))
-
-        # get the text surface dimensions
-        line1_width, line1_height = line1.get_size()
-        line2_width, line2_height = line2.get_size()
-
-        # calculate the position to center the text
-        display_info = pygame.display.Info()
-        x1 = display_info.current_w // 2 - line1_width // 2
-        y1 = display_info.current_h // 2 - (line1_height + line2_height) // 2
-        x2 = display_info.current_w // 2 - line2_width // 2
-        y2 = y1 + line1_height
-
-        # blit the text surfaces onto the screen surface
-        screen.blit(line1, (x1, y1))
-        screen.blit(line2, (x2, y2))
-
-        # update the screen
+    def open_window(self):
+        pygame.init()
+        screen_size = pygame.display.Info().current_w, pygame.display.Info().current_h
+        screen = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
+        screen.fill((0, 0, 0))
+        font = pygame.font.Font(None, 32)
+        text = ""
+        text = f"{self.current_break_type}: " if self.current_break_type else ""
+        text += self.get_random_exercise()
+        text_surface = font.render(text, True, (255, 255, 255))
+        # get the dimensions of the text surface
+        text_width, text_height = text_surface.get_size()
+        # calculate the center position for the text
+        x = (screen_size[0] / 2) - (text_width / 2)
+        y = (screen_size[1] / 2) - (text_height / 2)
+        # blit the text surface onto the center of the screen
+        screen.blit(text_surface, (x, y))
+        # update the display
         pygame.display.flip()
 
-    def pause_media_player(self):
+    def take_break(self, s):
+        logging.info(
+            f"Taking a {str(s if s < 60 else int(s/60))} {'second' if s < 60 else 'minute'} break"
+        )
+        # For true players only: vlc, mpv, RhythmBox, web browsers, cmus, mpd, spotify and others.
         subprocess.call(["playerctl", "pause"])
+        self.open_window()
+        os.system("aplay sound.wav")
+        time.sleep(s)
+        pygame.quit()
+        os.system("aplay sound.wav")
 
     def run(self):
-        if self.args.take_break:
-            self.take_break(int(self.args.take_break) * 60)
+        if self.args.break_now:
+            self.take_break(int(self.args.break_now) * 60)
 
-        count = self.short_freq_min * 60
+        count = self.short_break_freq_min * 60
         while True:
-            logging.info("Starting loop interation")
+            work_duration_seconds = self.short_break_freq_min * 60
+            # create a tqdm progress bar with the total duration
+            with tqdm(total=work_duration_seconds) as pbar:
+                # loop for the duration of the progress bar
+                for i in range(work_duration_seconds):
+                    # update the progress bar every second
+                    pbar.update(1)
+                    time.sleep(1)
 
-            logging.info(f"Sleeping for {self.short_freq_min} min")
-            time.sleep(self.short_freq_min * 60)
-
-            time_for_long = count % (self.long_freq_min * 60) == 0
+            time_for_long = count % (self.long_break_freq_min * 60) == 0
             logging.info(f"Taking a {'long' if time_for_long else 'short'} break")
 
             if time_for_long:
                 self.current_break_type = "Long break"
-                self.take_break(self.long_length_min * 60)
+                self.take_break(self.long_break_length_min * 60)
             else:
                 self.current_break_type = "Short break"
-                self.take_break(self.short_length_s)
+                self.take_break(self.short_break_length_sec)
 
-            count += self.short_freq_min * 60
+            count += self.short_break_freq_min * 60
 
 
-step_away = StepAway()
-step_away.run()
+def main():
+    step_away = StepAway()
+    step_away.run()
+
+
+if __name__ == "__main__":
+    main()
